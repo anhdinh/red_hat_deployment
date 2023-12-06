b1295d0b1ea6a006ff1c2bcb435673cb02ded494
Andy <dta89uct@gmail.com>
2023-12-06 20:22:21 +0700

    ad mongodb

--- src/main/java/com/example/demo/controller/HelloRedHatController.java
+++ src/main/java/com/example/demo/controller/HelloRedHatController.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.SystemDto;
+import com.example.demo.model.UserModel;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloRedHatController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public ResponseEntity<?> hello(){
-        SystemDto systemDto = new SystemDto();
-        systemDto.setMessage("Hello from andy with love");
-        systemDto.setStatus(200);
-        return ResponseEntity.ok(systemDto);
+        UserModel userModel = UserModel.builder().username("test").password("1234").birthDay(new Date()).build();
+        UserModel savedUser= userRepository.save(userModel);
+        return ResponseEntity.ok(savedUser);
     }
 }
